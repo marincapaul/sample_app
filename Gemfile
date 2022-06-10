066de635a4f0c4f192329de0fee9febc7bce190f
marincapaul <marincapaul@outlook.com>
2022-06-10 10:22:37 +0300

    Finish user sign-up

--- Gemfile
+++ Gemfile
@@ -62,7 +62,7 @@ group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
   # Use sqlite3 as the database for Active Record
   gem "sqlite3", "~> 1.4"
-  gem 'byebug', '11.0.1', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'byebug', '~> 11.0.1', platforms: [:mri, :mingw, :x64_mingw]
 end
 
 group :development do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create 
+    
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Greetings fellow sample man!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, 
+                                  :password_confirmation)
+    end
 end
