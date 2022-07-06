d2666caf7746c612376ac73af0c38a1a18e35cd7
marincapaul <marincapaul@outlook.com>
2022-07-06 21:16:42 +0300

    Add some config

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,13 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+   # config.active_storage.variant_processor = :mini_magick
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
